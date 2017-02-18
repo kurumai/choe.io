aed8893069818bcf0fa3a9a39d31962cdc4a86c1
Eugene Choe <eugene.choe@gmail.com>
2017-02-18 15:04:11 -0800

    Transfer copy from original to new boilerplate

--- client/components/about/index.jsx
+++ client/components/about/index.jsx
@@ -4,7 +4,16 @@ class About extends React.Component {
 	render() {
 		return (
 			<div className="block-text splash active">
-				ABOUT
+				<h1>About</h1>
+				<p>
+				I hold American, Canadian, and Korean passports, but really identify myself as a citizen of the world. Born in Toronto, I spent most of my formative years in the Bay Area. Fueled by a desire to explore an unknown city and an interest with working in a quantitative field, I left for New York University and graduated with a B.S. in Finance, after which I worked on trading floors for a few years—long enough to know that I had zero interest in working in the world of finance.
+				</p>
+				<p>
+				I started writing macros in VBA, which then turned into learning SQL and some Python as I got more comfortable with coding while working as a data analyst, and even as a chef (another story for another time). Along the way, I’ve discovered a passion for building applications that I’m unable to shake. I love what I do and am looking forward to all the cool things that I'll inevitably be a part of.
+				</p>
+				<p>
+				Let’s build something awesome together 🙌.
+				</p>
 			</div>
 		);
 	}
--- client/components/resume/index.jsx
+++ client/components/resume/index.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 class Resume extends React.Component {
 	render() {
 		return (
-			<div className="block-text splash active">
-				RESUME
+			<div class="block-text resume">
+				<a href="https://goo.gl/99TfvG" class="button resume-btn">Download PDF Resume</a><br/>
+				<iframe src="https://docs.google.com/document/d/1PfGjbEM0O8xz-dhwYrK_f8OUdl1KxfnRR2_QICrC0vg/pub?embedded=true" frameborder="0"></iframe>
 			</div>
 		);
 	}
