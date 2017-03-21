a98b1a3c1c5ab9c29e4a6e5ec9dcba9ca2cedb4f
Eugene Choe <eugene.choe@gmail.com>
2017-03-21 10:55:53 -0700

    Add GET route for static files

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/client'));
 
+app.get('*', function(req, res) {
+  return res.sendFile(__dirname + '/client/index.html');
+});
+
 console.log('listening on port 1337');
 app.listen(1337);
