fb80eb2e1bde3c597e910141a2c1dd7c47f7cb7a
Eugene Choe <eugene.choe@gmail.com>
2017-03-21 18:28:33 -0700

    Implement github repo cards

--- client/components/about/index.jsx
+++ client/components/about/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 import fetch from 'isomorphic-fetch';
 import './styles.css'
 
@@ -18,50 +19,27 @@ class About extends React.Component {
 			res = res.json();
 			return res;
 		}).then((data) => {
-			console.log(data);
 			return this.setState({repos: data});
 		});
 	}
 
-	// createRepoList() {
-	// 	if (!this.state.repos) {
-	// 		return (
-	// 			<li>
-	// 			Loading repos...
-	// 			</li>
-	// 		)
-	// 	}
-	// 	return (
-	// 		this.state.repos.map((repo) => {
-	// 			return (
-	// 				<li key={repo.id}>
-	// 					<a href={repo.html_url} target="_blank">{repo.name}</a>
-	// 					<br/>
-	// 					{repo.description ? repo.description : null}
-	// 					<br/>
-						
-	// 				</li>
-	// 			);
-	// 		})
-	// 	);
-	// }
-
 	createRepoList() {
 		if (!this.state.repos) {
 			return (
-				<li>
+				<div>
 				Loading repos...
-				</li>
+				</div>
 			)
 		}
 		return (
 			this.state.repos.map((repo) => {
 				return (
-					<li key={repo.id}>
-						<a href={repo.html_url} target="_blank">{repo.name}</a>
-						<br/>
-						{repo.description ? repo.description : null}
-					</li>
+					<div className="card" key={repo.id}>
+						<div className="card-block">
+							<h4 className="card-title"><a href={repo.html_url} target="_blank">{repo.name}</a></h4>
+							<p className="card-text">{repo.description ? repo.description : null}</p>
+						</div>
+					</div>
 				);
 			})
 		);
@@ -70,19 +48,25 @@ class About extends React.Component {
 	render() {
 		return (
 			<div className="about">
-				<h2>About</h2>
-				<p>
-				I hold American, Canadian, and Korean passports, but really identify myself as a citizen of the world. Born in Toronto, I spent most of my formative years in the Bay Area. Fueled by a desire to explore an unknown city and an interest with working in a quantitative field, I left for New York University and graduated with a B.S. in Finance, after which I worked on trading floors for a few years—long enough to know that I had zero interest in working in the world of finance.
-				</p>
-				<p>
-				I started writing macros in VBA, which then turned into learning SQL and some Python as I got more comfortable with coding while working as a data analyst, and even as a chef (another story for another time). Along the way, I’ve discovered a passion for building applications that I’m unable to shake. I love what I do and am looking forward to all the cool things that I'll inevitably be a part of.
-				</p>
-				<p>
-				Let’s build something awesome together 🙌.
-				</p>
-				<ul>
-					{ this.createRepoList() }
-				</ul>
+				<Grid>
+					<Row>
+						<Col sm={8} md={8} lg={8}>
+							<h2>About</h2>
+							<p>
+							I hold American, Canadian, and Korean passports, but really identify myself as a citizen of the world. Born in Toronto, I spent most of my formative years in the Bay Area. Fueled by a desire to explore an unknown city and an interest with working in a quantitative field, I left for New York University and graduated with a B.S. in Finance, after which I worked on trading floors for a few years—long enough to know that I had zero interest in working in the world of finance.
+							</p>
+							<p>
+							I started writing macros in VBA, which then turned into learning SQL and some Python as I got more comfortable with coding while working as a data analyst, and even as a chef (another story for another time). Along the way, I’ve discovered a passion for building applications that I’m unable to shake. I love what I do and am looking forward to all the cool things that I'll inevitably be a part of.
+							</p>
+							<p>
+							Let’s build something awesome together 🙌.
+							</p>
+						</Col>
+						<Col sm={4} md={4} lg={4}>
+							{ this.createRepoList() }
+						</Col>
+					</Row>
+				</Grid>
 			</div>
 		);
 	}
