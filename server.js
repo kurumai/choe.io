13600d7bd0dab0a646e084f98008bbc04ea3e0d1
Eugene Choe <eugene.choe@gmail.com>
2017-05-08 16:43:46 -0700

    Add rerouting for http to https

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
-const http = require('https');
+const http = require('http');
+const https = require('https');
 
 const app = express();
 
@@ -10,12 +11,21 @@ const options = {
 	cert: fs.readFileSync(sslPath + 'fullchain.pem')
 }
 
+function ensureSecure(req, res, next) {
+	if (req.secure) {
+		return next();
+	}
+	res.redirect('https://' + req.hostname + req.url);
+}
+
 app.use(express.static(__dirname + '/client'));
 
 app.get('*', function(req, res) {
   return res.sendFile(__dirname + '/client/index.html');
 });
 
-const server = http.createServer(options, app);  
-console.log('listening on port 443');
-server.listen(443);
+app.all('*', ensureSecure);
+
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);  
+console.log('SERVER OPERATIONAL');
