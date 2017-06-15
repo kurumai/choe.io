cf43bd830898cb05dab2773cbe01064880b37e9c
Eugene Choe <eugene@circleci.com>
2017-06-15 14:11:20 -0700

    Set up circleci 2.0 yaml Add nodemon to package.json

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const https = require('https');
 
 const app = express();
 
-const sslPath = '/etc/letsencrypt/live/choe.io/';
-const options = {
-	key: fs.readFileSync(sslPath + 'privkey.pem'),
-	cert: fs.readFileSync(sslPath + 'fullchain.pem')
-}
+// const sslPath = '/etc/letsencrypt/live/choe.io/';
+// const options = {
+// 	key: fs.readFileSync(sslPath + 'privkey.pem'),
+// 	cert: fs.readFileSync(sslPath + 'fullchain.pem')
+// }
 
 function ensureSecure(req, res, next) {
 	if (req.secure) {
@@ -24,8 +24,14 @@ app.get('*', function(req, res) {
   return res.sendFile(__dirname + '/client/index.html');
 });
 
-app.all('*', ensureSecure);
+// local dev port
+http.createServer(app).listen(1337);
 
-http.createServer(app).listen(80);
-https.createServer(options, app).listen(443);  
+
+// for production
+// app.all('*', ensureSecure);
+
+// http.createServer(app).listen(80);
+// https.createServer(options, app).listen(443);  
+
 console.log('SERVER OPERATIONAL');
