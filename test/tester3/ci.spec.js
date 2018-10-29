e2e150f02b859bd5f21391d6c44e7963b52080fa
Eugene Choe <eugene@circleci.com>
2018-10-29 10:29:13 +0900

    add more testing files

---
+++ test/tester3/ci.spec.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { expect } from 'chai';
+import { mount, shallow, render } from 'enzyme';
+import { Ci } from '../../client/components/ci/';
+
+describe('Trivial Application', function() {
+	describe('Button', function() {
+		it('tester3-1', function() {
+			const tester = mount(<Ci />);
+			expect(tester.state('showText')).to.equal(false);
+			tester.find('#btn-1').simulate('click');
+			expect(tester.state('showText')).to.equal(true);
+			tester.find('#btn-1').simulate('click');
+			expect(tester.state('showText')).to.equal(false);
+		});
+		it('tester3-2', function() {
+			const tester = mount(<Ci />);
+			expect(tester.find('#sentence').text()).to.equal('');
+			tester.find('#btn-1').simulate('click');
+			expect(tester.find('#sentence').text()).to.equal('CircleCI is Continuous Integration');
+		});
+		it('tester3-3', function() {
+			const tester = mount(<Ci />);
+			expect(tester.find('#sentence').text()).to.equal('');
+			tester.find('#btn-1').simulate('click');
+			setTimeout(function() { return 1; }, 3000);
+			expect(tester.find('#sentence').text()).to.equal('CircleCI is Continuous Integration');
+		});
+	});	
+});
